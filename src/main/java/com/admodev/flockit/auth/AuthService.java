bc23d7dc21d8d308d2a9d6fcd5583156f9afb8d5
admodev <admodevcodes@outlook.com>
2022-06-29 17:28:46 -0300

    add email validation

--- src/main/java/com/admodev/flockit/auth/AuthService.java
+++ src/main/java/com/admodev/flockit/auth/AuthService.java
@@ -1,4 +1,21 @@
 package com.admodev.flockit.auth;
 
+import com.admodev.flockit.Constants;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class AuthService {
+    private final Constants constants;
+
+    protected AuthService(Constants constants) {
+        this.constants = constants;
+    }
+
+    protected boolean validateEmail(String email) {
+        Pattern pattern = Pattern.compile(this.constants.EMAIL_REGEX_PATTERN);
+        Matcher matcher = pattern.matcher(email);
+
+        return matcher.matches();
+    }
 }
